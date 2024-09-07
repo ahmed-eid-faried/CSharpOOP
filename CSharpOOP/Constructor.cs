a05257bcbbb6b3f7857314001fdffcfde244991b
Ahmed Mady <96249012+ahmed-eid-faried@users.noreply.github.com>
2024-09-07 17:12:32 +0300

    ParameterizedConstructorEx

--- CSharpOOP/Constructor.cs
+++ CSharpOOP/Constructor.cs
@@ -6,7 +6,54 @@ using System.Threading.Tasks;
 
 namespace CSharpOOP
 {
-    internal class Constructor
+    class clsPerson
     {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public clsPerson()
+        {
+            Id = -1;
+            Name = "Empty";
+            Age = 0;
+        }
+    }
+    class Car
+    {
+
+        string brand;
+        int price;
+
+        // parameterized constructor
+        Car(string theBrand, int thePrice)
+        {
+
+            brand = theBrand;
+            price = thePrice;
+        }
+        internal class Constructor
+        {
+            public static void ConstructorEx()
+            {
+                clsPerson Person1 = new clsPerson();
+
+                Console.WriteLine("ID:= {0}", Person1.Id);
+                Console.WriteLine("Name:= {0}", Person1.Name);
+                Console.WriteLine("Age:= {0}", Person1.Age);
+                Console.ReadKey();
+            }
+            public static void ParameterizedConstructorEx()
+            {
+                // call parameterized constructor
+                Car car1 = new Car("Bugatti", 50000);
+
+                Console.WriteLine("Brand: " + car1.brand);
+                Console.WriteLine("Price: " + car1.price);
+                Console.ReadLine();
+            }
+
+        }
     }
 }
