402df776ab3fc49be4173f14da9616a952ab798e
Ahmed Mady <96249012+ahmed-eid-faried@users.noreply.github.com>
2024-09-08 23:06:48 +0300

    base keyword

--- CSharpOOP/Overriding.cs
+++ CSharpOOP/Overriding.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -25,6 +26,16 @@ namespace CSharpOOP
     }
     internal class clsOverriding
     {
+        //If the same method is present in both the base class and the derived class,
+        //the method in the derived class overrides the method in the base class. 
+        //This is called method overriding in C#.  
+
+
+        //In that case, we use the base keyword to call the method of the base class from the derived class.
+        //base keyword is used to call the Print method in the base class.
+
+
+
         public static void OverridingEx()
         {
             Console.WriteLine("================================");
